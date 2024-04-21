7448a3337b4fde0e29af842e9d57f1a0c0455a45
MiejkeHoltermann <miejke.holt@gmx.de>
2024-04-21 02:43:54 +0200

    add shopping cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { PokemonPageComponent } from './components/pages/pokemon-page/pokemon-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
   { path: 'tag/:tag', component: HomeComponent },
   { path: 'pokemon/:id', component: PokemonPageComponent },
+  { path: 'cart-page', component: CartPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { PokemonService } from './services/pokemon.service';
 import { PokemonPageComponent } from './components/pages/pokemon-page/pokemon-page.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,8 @@ import { TagsComponent } from './components/partials/tags/tags.component';
     SearchComponent,
     PokemonPageComponent,
     TagsComponent,
+    CartPageComponent,
+    TitleComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [PokemonService],
